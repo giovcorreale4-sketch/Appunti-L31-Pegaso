f6184b30b21740f0cccc5cd6902af357d6b0a8bc
giovcorreale4-sketch <giovcorreale4@gmail.com>
2026-04-13 15:50:37 +0200

    Update scripts.py

--- Secondo-Anno/Reti-di-calcolatori-e-Cybersecurity/scripts.py
+++ Secondo-Anno/Reti-di-calcolatori-e-Cybersecurity/scripts.py
@@ -1,14 +1,10 @@
 def cifrario_cesare(testo, shift):
     risultato = ""
-    for i in range(len(testo)):
-        char = testo[i]
+    for char in testo:
         if char.isupper():
-            risultato += chr((ord(char) + shift - 65) % 26 + 65)
+            risultato += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
         elif char.islower():
-            risultato += chr((ord(char) + shift - 97) % 26 + 97)
+            risultato += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
         else:
             risultato += char
     return risultato
-
-print(cifrario_cesare("Attacco alle ore dieci", 3)) 
-# Output: Dwwdffr dood ruh glhfl
